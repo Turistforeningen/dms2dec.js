181663210176758cea10fb232191258b262cb5ca
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-07-30 09:02:28 +0200

    No more "should" in test suite

--- test.js
+++ test.js
@@ -10,50 +10,49 @@ describe('dms2dec', function() {
     var lonRef = 'E';
     var lonDec = 5.370986111111111;
 
-  it('should parse dms strings', function() {
+  it('parses dms strings', function() {
     assert.deepEqual(dms2dec(lat, latRef, lon, lonRef), [latDec, lonDec]);
   });
 
-  it('should parse dms strings without commas', function() {
+  it('parses dms strings without commas', function() {
     var lat = '60/1 21/1 4045/100';
     var lon = '5/1 22/1 1555/100';
 
     assert.deepEqual(dms2dec(lat, latRef, lon, lonRef), [latDec, lonDec]);
   });
 
-  it('should parse dms strings without spaces', function() {
+  it('parses dms strings without spaces', function() {
     var lat = '60/1,21/1,4045/100';
     var lon = '5/1,22/1,1555/100';
 
     assert.deepEqual(dms2dec(lat, latRef, lon, lonRef), [latDec, lonDec]);
   });
 
-  it('should parse dms arrays of strings', function() {
+  it('parses dms arrays of strings', function() {
     var lat = ['60/1', '21/1', '4045/100'];
     var lon = ['5/1' , '22/1', '1555/100'];
 
     assert.deepEqual(dms2dec(lat, latRef, lon, lonRef), [latDec, lonDec]);
   });
 
-  it('should parse dms arrays of numbers', function() {
+  it('parses dms arrays of numbers', function() {
     var lat = [60, 21, 40.45];
     var lon = [5 , 22, 15.55];
 
     assert.deepEqual(dms2dec(lat, latRef, lon, lonRef), [latDec, lonDec]);
   });
 
-  it('should parse western coorinates', function() {
+  it('parses western coorinates', function() {
     var lonRef = 'W';
     var lonDec = -5.370986111111111;
 
     assert.deepEqual(dms2dec(lat, latRef, lon, lonRef), [latDec, lonDec]);
   });
 
-  it('should parse southern coorinates', function() {
+  it('parses southern coorinates', function() {
     var latRef = 'S';
     var latDec = -60.36123611111111;
 
     assert.deepEqual(dms2dec(lat, latRef, lon, lonRef), [latDec, lonDec]);
   });
 });
-
